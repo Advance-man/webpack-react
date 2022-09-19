39a37269f21a295c6278e65da727da5f4dc996eb
gaojingtao <729007572@qq.com>
2022-09-19 20:14:55 +0800

    使用class编写单例模式

---
+++ src/utils/signleton/singleton1.js
@@ -0,0 +1,22 @@
+// 单例模式
+export class Signleton{
+  static instance = null
+  constructor(name){
+    if(Signleton.instance){
+      return Signleton.instance;
+    }
+    this.name = name;
+    Signleton.instance = this;
+  }
+  
+  getName(){
+    console.log(this.name);
+  return this.name;
+  }
+}
+
+let per1 = new Signleton("lily");
+let per2 = new Signleton('lucy');
+per1.getName();  //lily
+per2.getName();  //lily
+console.log("per1 === per2",per1 === per2) //per1 === per2 true
